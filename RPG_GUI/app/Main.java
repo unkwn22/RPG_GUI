27fd84baa1eb3cc79ff0504572ee87d9b5a389ac
Jason Choi <lsjc12911@naver.com>
2021-08-03 09:54:45 +0900

    feat: Java swing importation with JFrame constructed

--- RPG_GUI/app/Main.java
+++ RPG_GUI/app/Main.java
@@ -1,5 +1,6 @@
 package app;
 
+import java.awt.*;
 import java.util.ArrayList;
 
 import constructor.ItemConstructor;
@@ -8,7 +9,30 @@ import entity.*;
 import handler.Start;
 import handler.Town;
 
+import javax.swing.*;
+
 public class Main {
+
+	//Frame 생성
+	JFrame window;
+	Container con;
+	//제목
+	JPanel titleNamePanel;
+	//시작 버튼
+	JPanel startButtonPanel;
+
+	//제목 Label 생성
+	JLabel titleNameLabel;
+	//시작 버튼 Label 생성
+	JLabel startButtonLabel;
+	//시작 Button 생성
+	JButton startButton;
+
+	//제목 Font 지정
+	Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+	//시작 Font 지정
+	Font startFont = new Font("Times New Roman", Font.PLAIN, 50);
+
 	Start s = new Start();
 	Human player = null;
 	ItemConstructor i = new ItemConstructor();
@@ -26,6 +50,40 @@ public class Main {
 	ArrayList<Monster> monster = null;
 	
 	public Main() {
+
+		//JFrame 기본 설정
+		window = new JFrame();
+		window.setSize(800, 600);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.getContentPane().setBackground(Color.BLACK);
+		window.setLayout(null);
+		window.setVisible(true);
+
+		//JFrame 생성시 JFrame이 지니고 있는 container를 사용 하겠다고 생성
+		con = window.getContentPane();
+
+		//제목 Panel 생성
+		titleNamePanel = new JPanel();
+		titleNamePanel.setBounds(100, 100, 600, 150);
+		titleNamePanel.setBackground(Color.BLACK);
+		//Panel 안에 들어갈 문구 (Label) 생성
+		titleNameLabel = new JLabel("Runescape");
+		titleNameLabel.setForeground(Color.white);
+		titleNameLabel.setFont(titleFont); //위에 지정 해놓은 font로 설정
+		titleNamePanel.add(titleNameLabel);
+		con.add(titleNamePanel);
+
+		//시작 버늩 Panel 생성
+		startButtonPanel = new JPanel();
+		startButtonPanel.setBounds(100, 400, 600, 100);
+		startButtonPanel.setBackground(Color.BLACK);
+		startButton = new JButton();
+		startButtonLabel = new JLabel("Start");
+		startButtonLabel.setFont(startFont);
+		startButton.add(startButtonLabel);
+		startButtonPanel.add(startButton);
+		con.add(startButtonPanel);
+
 		player = s.characterSettings();
 		weapon = i.initWeapons();
 		potion = i.initPotions();
